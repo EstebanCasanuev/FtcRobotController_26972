7e088a4fc2a5865ed4caadd54f06c843301a33de
EstebanCasanuev <carlosesteban.casanueva@outlook.com>
2025-01-06 12:24:44 -0800

    Added wrist

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -22,13 +22,17 @@ import org.firstinspires.ftc.robotcore.external.JavaUtil;
 @TeleOp(name = "Main")
 public class Main extends LinearOpMode {
 
-    public static double P_SLIDEMOTION= 0.1;
-    public static double I_SLIDEMOTION= 0.00;
-    public static double D_SLIDEMOTION= 0.00;
+    public static double P_SLIDEMOTION= 1.5;
+    public static double I_SLIDEMOTION= 0.001;
+    public static double D_SLIDEMOTION= 0.01;
 
     public static double P_SWINGMOTION= 0.1;
     public static double I_SWINGMOTION= 0.00;
     public static double D_SWINGMOTION= 0.00;
+
+    public static double P_WRIST= 0.1;
+    public static double I_WRIST= 0.00;
+    public static double D_WRIST = 0.00;
 
     private HolonomicOdometry odometry;
 
@@ -40,13 +44,9 @@ public class Main extends LinearOpMode {
     public static final double DISTANCE_PER_PULSE = Math.PI * WHEEL_DIAMETER / TICKS_PER_REV;
 
     private MotorEx frontRightMotor, rearRightMotor, frontLeftMotor, rearLeftMotor;
-    private MotorEx rightSlider, leftSlider;
-    //private MotorEx SlideMotion;
+    private MotorEx rightSlider, leftSlider, Wrist, SlideMotion;
 
 
-
-    //ColorSensor colorSensor;
-    //SensorColor colorSensor;
     RevColorSensorV3 colorSensor;
         NormalizedRGBA myNormalizedColors;
         int myColor;
@@ -56,19 +56,19 @@ public class Main extends LinearOpMode {
         float saturation;
         float value;
 
-    //PIDController SlidePID = new PIDController(P_SLIDEMOTION, I_SLIDEMOTION, D_SLIDEMOTION);
+    PIDController SlidePID = new PIDController(P_SLIDEMOTION, I_SLIDEMOTION, D_SLIDEMOTION);
     //PIDController SwingSlidePID = new PIDController(P_SWINGMOTION, I_SWINGMOTION, D_SWINGMOTION);
+    PIDController WristPID = new PIDController(P_WRIST, I_WRIST, D_WRIST);
 
     MecanumDrive m_Drive;
     @Override
     public void runOpMode() {
 
+        SlidePID.setTolerance(100);
 
-        //SlidePID.setTolerance(100);
+        //colorSensor = hardwareMap.get(RevColorSensorV3.class, "colorSensor");
 
-        //colorSensor = new SensorColor(hardwareMap, "colorSensor");
-        colorSensor = hardwareMap.get(RevColorSensorV3.class, "colorSensor");
-        frontLeftMotor = new MotorEx(hardwareMap, "frontLeftMotor");
+        /*frontLeftMotor = new MotorEx(hardwareMap, "frontLeftMotor");
         frontRightMotor = new MotorEx(hardwareMap, "frontRightMotor");
         rearRightMotor = new MotorEx(hardwareMap, "rearRightMotor");
         rearLeftMotor = new MotorEx(hardwareMap, "rearLeftMotor");
@@ -95,13 +95,14 @@ public class Main extends LinearOpMode {
                 CENTER_WHEEL_OFFSET
         );*/
 
-        //rightSlider = new MotorEx(hardwareMap, "rightSlider");
-        //leftSlider = new MotorEx(hardwareMap, "leftSwing");
+        //rightSlider = new MotorEx(hardwareMap, "Slider");
+
+        Wrist = new MotorEx(hardwareMap, "Wrist");
 
         //SlideMotion = new MotorEx(hardwareMap, "SlideMotion");
 
         //rightSlider.resetEncoder();
-        //leftSlider.resetEncoder();
+        Wrist.resetEncoder();
         //SlideMotion.resetEncoder();
 
         //leftSlider.setInverted(true);
@@ -110,7 +111,7 @@ public class Main extends LinearOpMode {
 
         //telemetry.addData("Robot Position at Init: ", odometry.getPose());
 
-        telemetry.addData("Color Sensor Enabled: ", colorSensor.initialize());
+        //telemetry.addData("Color Sensor Enabled: ", colorSensor.initialize());
         telemetry.update();
 
         waitForStart();
@@ -119,48 +120,66 @@ public class Main extends LinearOpMode {
             while (opModeIsActive()) {
                 //odometry.updatePose();
 
-                m_Drive.driveRobotCentric(
+                /*m_Drive.driveRobotCentric(
                         gamepad1.left_stick_x,
                         -gamepad1.left_stick_y,
                         gamepad1.right_stick_x
-                );
+                );*/
 
-                //SlideMotion.set(SlidePID.calculate(SlideMotion.getCurrentPosition()) *0.2);
                 //rightSlider.set(SwingSlidePID.calculate(leftSlider.getCurrentPosition()));
                 //leftSlider.set(SwingSlidePID.calculate(leftSlider.getCurrentPosition()));
+                Wrist.set(WristPID.calculate(Wrist.getCurrentPosition()));
+                telemetry.addData("Wrist Encoder: ", Wrist.getCurrentPosition());
+                telemetry.addData("Wrist Power: ", WristPID.calculate(Wrist.getCurrentPosition()));
+                telemetry.addData("Wrist Setpoint: ", WristPID.getSetPoint());
+
+
+                /*if (gamepad2.dpad_down) {
+                    SlideMotion.set(gamepad2.right_trigger - gamepad2.left_trigger);
+                } else {
+                    SlideMotion.set(SlidePID.calculate(SlideMotion.getCurrentPosition()));
+                }*/
+
+                if (gamepad2.left_bumper) {
+                    WristPID.setSetPoint(-100);
+                } else {
+                    WristPID.setSetPoint(-10);
+                }
+
+
                 //telemetry.addData("Encoder: ", SlideMotion.getCurrentPosition());
 
-                myNormalizedColors = ((NormalizedColorSensor) colorSensor).getNormalizedColors();
+                /*myNormalizedColors = ((NormalizedColorSensor) colorSensor).getNormalizedColors();
                 myColor = myNormalizedColors.toColor();
                 hue = JavaUtil.rgbToHue(Color.red(myColor), Color.green(myColor), Color.blue(myColor));
                 saturation = JavaUtil.rgbToSaturation(Color.red(myColor), Color.green(myColor), Color.blue(myColor));
                 value = JavaUtil.rgbToValue(Color.red(myColor), Color.green(myColor), Color.blue(myColor));
 
-                if(red() > blue() && red() > green()){
+                if (red() > blue() && red() > green()) {
                     ActualColor = "red";
                 } else if (green() > blue() && green() > red()) {
                     ActualColor = "green";
-                }else if (blue() > green() && blue() > red()) {
+                } else if (blue() > green() && blue() > red()) {
                     ActualColor = "blue";
                 }
-                    telemetry.addData("Color: ", ActualColor);
+                telemetry.addData("Color: ", ActualColor);
 
-                telemetry.update();
 
-                /*if(gamepad2.a){
-                    SlidePID.setSetPoint(-1460);
+                /*if (gamepad2.a) {
+                    SlidePID.setSetPoint(-8500);
                     //SwingSlidePID.setSetPoint(0);
-                } else if (gamepad2.b){
+                } else if (gamepad2.b) {
                     SlidePID.setSetPoint(-80);
                     //SwingSlidePID.setSetPoint(500);
                 } else if (gamepad2.x) {
                     //SwingSlidePID.setSetPoint(8000);
-                    SlidePID.setSetPoint(-1000);
+                    SlidePID.setSetPoint(-4000);
                 }*/
 
                 /*telemetry.addData("X:", odometry.getPose().getX());
                 telemetry.addData("Y:", odometry.getPose().getY());
                 telemetry.addData("Angulo:", odometry.getPose().getRotation());*/
+                telemetry.update();
             }
         }
     }
